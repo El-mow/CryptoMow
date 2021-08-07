9ee0a8c23688885eb0ce00e71796fcac08a6d3eb
mobinezzati662 <mobinezzati662@gmail.com>
2021-08-07 15:45:27 -0500

    Initial Commit

---
+++ CryptoMow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoMow
+//
+//  Created by mobin on 8/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoMow/CryptoMowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMowApp.swift
+//  CryptoMow
+//
+//  Created by mobin on 8/7/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
